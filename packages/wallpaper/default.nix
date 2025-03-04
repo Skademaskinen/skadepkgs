1ab73c348f079f9aa72331c7cfd47b2ed389327b
Thomas Jensen <mast3r@skade.dev>
2025-03-04 20:35:51 +0100

    fixed hashes

--- packages/wallpaper/default.nix
+++ packages/wallpaper/default.nix
@@ -1,19 +1,19 @@
-{ pkgs }: let
+{ pkgs, wallpaper ? pkgs.fetchurl {
+    
+    url = "https://img.goodfon.com/original/3840x2160/c/8d/anime-mech-fate.jpg";
+    hash = "sha256-my/d2+nKKj+XCtgS5hC2mKFb7yIvVNg9P1IelPl9lRY=";
+}}: let
     env = pkgs.python312.withPackages (py: with py; [
         pillow
         cairosvg
     ]);
-    background = pkgs.fetchurl {
-        url = "https://img.goodfon.com/original/3840x2160/c/8d/anime-mech-fate.jpg";
-        hash = "sha256-my/d2+nKKj+XCtgS5hC2mKFb7yIvVNg9P1IelPl9lRY=";
-    };
     images = [
         "${pkgs.nixos-artwork.wallpapers.simple-dark-gray-bootloader}/share/backgrounds/nixos/nix-wallpaper-simple-dark-gray_bootloader.png"
         "${pkgs.fetchFromGitHub {
             owner = "WillPower3309";
             repo = "swayfx";
-            rev = "master";
-            sha256 = "sha256-+yHlfDX0fLXe5LFAwiK/Avtf0vVtqhYfjq7fWcoGIX4=";
+            rev = "0.5";
+            sha256 = "sha256-gdab7zkjp/S7YVCP1t/OfOdUXZRwNvNSuRFGWEJScF8=";
         }}/assets/swayfx_logo.svg"
         "${pkgs.fetchFromGitHub {
             owner = "f-klubben";
@@ -27,6 +27,6 @@ in pkgs.stdenv.mkDerivation {
     pname = "wallpaper.png";
     src = ./.; 
     installPhase = ''
-        ${env.interpreter} $src/overlay.py ${background} ${builtins.concatStringsSep " " images} $out
+        ${env.interpreter} $src/overlay.py ${wallpaper} ${builtins.concatStringsSep " " images} $out
     '';
 }
