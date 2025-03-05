d3f75d760a1640d9b6268475b2878b8a4fb4cbbc
Thomas Jensen <mast3r@skade.dev>
2025-03-05 02:09:09 +0100

    added desktop and server tests

---
+++ test/desktop/default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+    skade.users.banner.enable = true;
+    users.users.root.password = "1234";
+
+    networking.hostName = "desktop";
+    system.stateVersion = "24.11";
+}
---
+++ test/server/default.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+    skade.users.banner.enable = true;
+    users.users.root.password = "1234";
+    virtualisation.vmVariant.virtualisation.graphics = false;
+
+    networking.hostName = "server";
+    system.stateVersion = "24.11";
+}
